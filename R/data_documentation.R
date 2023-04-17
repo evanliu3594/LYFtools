9607fb21123311f7ad85edc70679dc13c559cb90
yifan.liu <yifan.liu@smail.nju.edu.cn>
2023-04-17 16:43:26 +0800

    add more china map componects

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -1,14 +1,31 @@
-#' Regex-Syntax for subsetting all Chinese characters
+#' Regex-Syntax for subsetting all Chinese characters.
 #'
 #' @format ## `all_cn_chr`
-#' a chr string of regex syntax
-#'
+#' a chr string of regex syntax to match all Chinese characters.
 #'
 "all_cn_chr"
 
-#' PROJ4 string of China projection coordinate system
+#' PROJ4 string of China projection coordinate system.
 #'
 #' @format ## `cncrs`
-#' a string of PROJ4 albert projection for China domain
+#' a string of PROJ4 Albert projection for China domain.
 #'
 "cncrs"
+
+#' Nine dotted lines for China south sea boundary.
+#'
+#' @format a sf line object,
+#'
+"cnmap_9dx"
+
+#' China map in county scale, imported and simplified form `mapchina` package.
+#'
+#' @format a sf polygon object.
+#'
+"cnmap_counties"
+
+#' islands of China in far sea which are not included in county-scale map.
+#'
+#' @format a sf polygon object.
+#'
+"cnmap_islands"
