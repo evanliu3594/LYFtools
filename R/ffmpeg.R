8dfe3ad28134c7b745d41f64bd3451f0ca404690
yifan.liu <yifan.liu@smail.nju.edu.cn>
2023-06-13 02:56:19 +0800

    modify ffmpeg_video.comp

--- R/ffmpeg.R
+++ R/ffmpeg.R
@@ -1,25 +1,48 @@
 #' Call ffmpeg in shell to compress video
 #'
 #' @param video video path, full path.
-#' @param resolution portrait video picture size, i.e., 480, 720, 1080, 1440, 2160.
-#' @param bit_rate bit rate, unit:k.
-#' @param encoder video encoder, by default `hevc_nvenc` for nvidia GPU. alternative `hevc_amf` for AMD GPU. `libx264` for CPU encoding only.
+#' @param res resolution, numeric. especially portrait video picture size, i.e., 480, 720, 1080, 1440, 2160.
+#' By default NULL for not changing bit rate.
+#' @param bv video bit rate, unit: kb. By default NULL for not changing bit rate.
+#' @param cv video encoder, by default `hevc_nvenc` for nvidia GPU hwa. alternative `hevc_amf` for AMD GPU hwa.
+#' `libx264` for CPU encoding only.
+#' @param force_format Output file format, chr. By default NULL for keeping file format as input file.
 #' @param extra extra option pass to ffmpeg.
+#'
 #' @importFrom stringr str_extract
+#' @importFrom jsonlite fromJSON
 #'
 #' @return status for compile.
 #' @export
 #'
 #' @examples
 #' ffmpeg_video.compress("path/to/your/video")
-ffmpeg_video.compress <- function(video, resolution = 720, bit_rate = 2000, encoder = "hevc_nvenc", extra = "") {
+ffmpeg_video.compress <- function(video, res = NULL, bv = NULL, force_format = NULL, cv = "hevc_nvenc", extra = "") {
 
-  filenm <- get_fname(video)
+  input_res <- str_glue(
+      "ffprobe \"{video}\" -v quiet -select_streams v:0 -show_entries stream=width,height -of json"
+    ) %>% system(intern = T) %>% fromJSON() %>% .$stream %>% c()
 
-  dirnm <- dirname(video)
+  fnm <- get_fname(video)
 
-  file_ext <- str_extract(video, "(?<=\\.)[^\\.]+$")
+  dnm <- dirname(video)
 
-  str_glue("ffmpeg -i \"{video}\" -vf scale=-1:{resolution} -b:v {bit_rate}k -c:v {encoder} {extra} \\
-           \"{dirnm}/{filenm}[{resolution}P][{bit_rate}k].{file_ext}\"") |> system()
+  cmd_res_str <- if (is.null(res)) "" else str_glue("-vf scale=-1:{res}")
+
+  cmd_bv_str <- if (is.null(bv)) "" else str_glue("-b:v {bv}k")
+
+  cmd_cv_str <- str_glue("-c:v {cv}")
+
+  fnm_res_str <- if (is.null(res)) {
+    str_glue("[{input_res$width}x{input_res$height}]")
+  } else {
+    str_glue("[{as.integer(input_res$width * res / input_res$height)}x{res}]")
+  }
+
+  fnm_bv_str <- if (is.null(bv)) "" else str_glue("[{bv}k]")
+
+  fnm_ext <- if (is.null(force_format)) str_extract(video, "(?<=\\.)[^\\.]+$") else force_format
+
+  str_glue("ffmpeg -i \"{video}\" {cmd_res_str} {cmd_bv_str} {cmd_cv_str} {extra} \\
+           \"{dnm}/{fnm}{fnm_res_str}{fnm_bv_str}.{fnm_ext}\"") |> system()
 }
